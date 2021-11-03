48fecd7e463b06308f3f63be511701694270def6
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-02 20:03:36 -0600

    Obtener productos por su categoria

--- src/api/products.js
+++ src/api/products.js
@@ -13,3 +13,18 @@ export async function getProductsAPI(limit= 1000){
         return null
     }
 }
+
+export async function getProductCategory(category){
+    try {
+        const response= await fetch(
+            `${API_URL}/products?_where[category.slug]=${category}&_sort=created_at:desc`
+        );
+
+        const result= await response.json();
+        return result;
+        
+    } catch (error) {
+        console.log(error);
+        return null
+    }
+}
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -6,6 +6,7 @@
 
 <script>
 import BasicLayout from '../layouts/BasicLayout.vue';
+import { getProductCategory } from '../api/products';
 
 export default {
     name: "Category",
@@ -17,6 +18,10 @@ export default {
         $route(to, from){
             console.log(to);
         }
+    },
+    async created(){
+        const products= await getProductCategory("golosinas");
+        console.log(products);
     }
 }
 </script>
