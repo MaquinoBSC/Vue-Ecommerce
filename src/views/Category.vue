a0f0b7f049fd6413cff2ea28b13df60829b82b37
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-04 15:46:17 -0600

    Obtener los productos de acuerdo a la categoria de manera dinamica

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,27 +1,39 @@
 <template>
     <BasicLayout>
         <h1>Estamos en la categoria</h1>
+        {{ products }}
     </BasicLayout>
 </template>
 
 <script>
 import BasicLayout from '../layouts/BasicLayout.vue';
 import { getProductCategory } from '../api/products';
+import Products from '../components/Products.vue';
 
 export default {
     name: "Category",
     components: {
-        BasicLayout
+        BasicLayout,
+        Products
     }, 
     watch: {
         //De esta manera podemos acceder al parametro que se envia en una ruta dinamica
         $route(to, from){
-            console.log(to);
+            this.getProducts(to.params.category);
         }
     },
-    async created(){
-        const products= await getProductCategory("golosinas");
-        console.log(products);
+    created(){
+    },
+    data(){
+        return{
+            products: [],
+        }
+    },
+    methods: {
+        async getProducts(category){
+            this. products= await getProductCategory(category);
+            console.log(this.products);
+        }
     }
 }
 </script>
